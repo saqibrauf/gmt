ee244dd24a8729fa96a7a610d6704aa4fda21a83
saqibrauf <saqibrauf@yahoo.com>
2018-09-08 17:46:19 +0500

    Heavy

--- mprices/views.py
+++ mprices/views.py
@@ -1,20 +1,30 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from .models import Location, Brand, Phone
 
 def index(request):
-	phones = Phone.objects.filter(price__gt=0).order_by('-release', '-price')
+	if request.POST:
+		minPrice = request.POST.get('min-price', '10000')
+		maxPrice = request.POST.get('max-price', '30000')
+		phones = Phone.objects.filter(price__gte=minPrice, price__lte=maxPrice).order_by('-price')
+		message = 'Search result for mobiles between ' + minPrice + ' to ' + maxPrice
+	else:
+		phones = Phone.objects.filter(price__gt=0).order_by('-release', '-price')
+		message = ''
+
 	sidebar_brands = Brand.objects.all().order_by('brand_name')
 	context = {
 		'phones' : phones,
 		'sidebar_brands' : sidebar_brands,
+		'message' : message
 	}
 	return render(request, 'mprices/index.html', context)
 
 def brand(request, slug, location):
-	brand = Brand.objects.get(brand_slug=slug)
+	brand = get_object_or_404(Brand, brand_slug=slug)
 	phones = brand.phone_set.all().order_by('-price')
 	sidebar_brands = Brand.objects.all().order_by('brand_name')
-	sidebar_location = Location.objects.get(location_slug=location)
+	sidebar_location = get_object_or_404(Location, location_slug=location)
 	context = {
 		'brand' : brand,
 		'phones' : phones,
@@ -24,11 +34,11 @@ def brand(request, slug, location):
 	return render(request, 'mprices/brand.html', context)
 
 def phone(request, slug, location):
-	phone = Phone.objects.get(phone_model_slug=slug)
+	phone = get_object_or_404(Phone, phone_model_slug=slug)
 	sidebar_brands = Brand.objects.all().order_by('brand_name')
 	brand = phone.brand_name
 	sidebar_phones = Phone.objects.filter(brand_name=brand)
-	sidebar_location = Location.objects.get(location_slug=location)
+	sidebar_location = get_object_or_404(Location, location_slug=location)
 	context = {
 		'phone' : phone,
 		'brand' : brand,
