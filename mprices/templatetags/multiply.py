6059a5fcb851557cbd970c11f15e67da8299f32b
Saqib Rauf <saqibrauf@yahoo.com>
2018-10-12 15:33:17 +0500

    chaskay

--- mprices/templatetags/multiply.py
+++ mprices/templatetags/multiply.py
@@ -3,6 +3,6 @@ from django import template
 register = template.Library()
 
 @register.simple_tag
-def multiply(usd, exchange_rate, *args, **kwargs):
-    rate = usd * exchange_rate
+def multiply(price, exchange_rate, *args, **kwargs):
+    rate = price * exchange_rate   
     return int(rate) #round(int(rate),-2)
