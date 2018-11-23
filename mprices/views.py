0524e3f96742966351099614fc1d46fa5a9d0912
saqib Rauf <saqibrauf@yahoo.com>
2018-11-23 12:03:45 +0500

    25478147

--- mprices/views.py
+++ mprices/views.py
@@ -57,7 +57,7 @@ def search(request):
 	if request.GET:
 		min_price = request.GET.get('minprice', '100')
 		max_price = request.GET.get('maxprice', '250')
-		phones = Phone.objects.filter(price__gte=min_price, price__lte=max_price).order_by('-release', '-price')
+		phones = Phone.objects.filter(price__gte=min_price, price__lte=max_price).order_by('-price')
 		total = str(phones.count())
 		message = total + ' Search result for mobiles between ' + min_price + ' to ' + max_price
 		
